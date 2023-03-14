4ded73ec473d0d568baf54d0a8243dd9ca3fecd6
Juwon <dnladesanya1@gmail.com>
2023-03-14 14:01:02 +0100

    fixed linters error

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def initializer( id, name = 'Unknown', age, parent_permission = 'true')
+  def initializer(id, name = 'Unknown', age, parent_permission: 'true')
     @id = id
     @name = name
     @age = age
