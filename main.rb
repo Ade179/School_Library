d1fcbbf54408296053d4bb1489d64138341c9def
Abel Seifu <abel.workmail@gmail.com>
2023-03-21 11:53:53 +0300

    Refactor main.rb

--- main.rb
+++ main.rb
@@ -1,17 +1,15 @@
 require_relative 'runapp'
+require_relative 'endapp'
 
 class Main
   def initialize
     start = StartApp.new
     start.menu_options
   end
-
-  def end_app
-    puts ' '
-    puts 'Thank you for using The Gate School Library App!'
-    puts ' '
+  def close_app
+    EndApp.new
   end
 end
 
 main = Main.new
-main.end_app
+main.close_app
